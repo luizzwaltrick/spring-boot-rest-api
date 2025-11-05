7ae1684846e393772c72eac9581c0a130d6bb919
Luiz Waltrick <240g4hp@gmail.com>
2025-11-05 09:26:18 -0300

    feat: create task validation and exception handling

---
+++ src/main/java/com/github/luizzwaltrick/todo/todo_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.github.luizzwaltrick.todo.todo_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/com/github/luizzwaltrick/todo/todo_api/task/TodoController.java
+++ src/main/java/com/github/luizzwaltrick/todo/todo_api/task/TodoController.java
@@ -1,7 +1,6 @@
 package com.github.luizzwaltrick.todo.todo_api.task;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
